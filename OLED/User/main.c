3bd8633edf38c4d176bc69292ea43e7103e0e4e4
Emerson0102a <w15150055831@163.com>
2025-05-19 22:46:04 +0800

    OLED，项目起点

---
+++ OLED/User/main.c
@@ -0,0 +1,35 @@
+/*说明：
+A1 电源
+A0 38kHz载波
+
+
+*/
+
+
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+
+
+
+
+
+int main(void)
+{
+
+	
+
+	
+	OLED_ShowString(1,1,"wuchangsheng");
+	OLED_ShowString(2,1,"2024080905006");
+	
+	while(1)
+	{
+		
+		
+		
+		
+		
+		
+	}
+}
